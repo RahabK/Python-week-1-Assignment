ecd55800880ff44e28677235a95612bc7bd843e2
RahabK <rahabkibanya@gmail.com>
2024-11-06 12:17:25 +0300

    Week 1

--- user_input.py
+++ user_input.py
@@ -2,11 +2,10 @@
 name = input("What's your name? ")
 
 # Ask for the user's age and store it in the variable "age"
-age = input("How old are you? ")  
+age = input("How old are you? ")
 
 # Ask for the user's location and store it in the variable "location"
 location = input("Where do you live? ")
 
 # Print a personalized message
-print(f"Hello [{name}], you are [{age}] years old and live in [{location}].")
-
+print(f"Hello {name}, you are {age} years old and live in {location}.")
